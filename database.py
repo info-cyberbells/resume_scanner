6c20f12ead7eedd7d73c9bf5564fea2319ba3f62
riya112004 <riyakatoch07@gmail.com>
2026-01-21 13:33:05 +0530

    updated datbase

--- database.py
+++ database.py
@@ -5,10 +5,9 @@ from bson.objectid import ObjectId
 from models import Resume
 
 # --- Database Configuration ---
-# Replace with your MongoDB connection string
-MONGO_DETAILS = os.environ.get("MONGO_DETAILS", "mongodb://localhost:27017")
+MONGO_DETAILS = "mongodb+srv://infocyberbells:URgCpmEAgksetiiI@cyberbellsmongocluster.vy8xm.mongodb.net/resumes?retryWrites=true&w=majority"
 client = MongoClient(MONGO_DETAILS)
-database = client.resumes
+database = client.get_database()  
 resume_collection = database.get_collection("resume_collection")
 
 
